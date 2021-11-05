ce3acf7ea37524138560eab22d7858468098ea34
ahoerr2 <ahoerr2@gmail.com>
2021-11-05 13:09:37 -0500

    Added Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,107 @@
+// lab04.cpp
+/* Simple process scheduler simulator program */
+/* include c header files */
+#include <stdlib.h>
+#include <unistd.h> // for function fork()
+#include <stdio.h>
+#include <time.h> //for generate random seed
+// include c++ header files
+#include <string>
+#include <iostream>
+#define N 3 //N is the number of the worker processes. You may increase N to 100 when your program runs correctly
+#define M 3 //M is the number of jobs. You may increase M to 50 when your program runs correctly
+
+using namespace std;
+
+void setJobQueues();
+void jobGenerator();
+void jobScheduler();
+int selectJob();
+void executeJob(int n);
+
+int main()
+{
+    int pid = 0;
+    setJobQueues(); /* Set up the priority job queues with chosen file and/or data structure */
+    if (pid = fork() > 0)
+    {                   /* jobGenerator process */
+        jobGenerator(); /* generate random jobs and put them into the priority queues. The priority queues must be protected in a critical region */
+        exit(0);
+    }
+    else
+    {                   /* job scheduler process */
+        jobScheduler(); /* schedule and execute the jobs. */
+        exit(0);
+    }
+    return (1);
+}
+
+void setJobQueues()
+{
+    cout << "Main: Set up the job priority queue: \n";
+    /* ... */
+}
+
+void jobGenerator()
+{
+    int i = 0, n = 0;
+    cout << "jobGenerator: Use a loop to generate M jobs in the priority queue: \n";
+    // initialize random seed
+    srand(time(0));
+    while (i < M)
+    {
+        // generate a random number between 1-100
+        n = rand() % 100 + 1;
+        cout << "jobGenerator: Job number is : " << n << endl;
+        // Put the job n into the priority queue
+        /* ... */
+
+        usleep(100); //100 can be adjusted to synchronize the job generation and job scheduling processes.
+        i++;
+    }
+}
+
+void jobScheduler()
+{
+    int i = 0, n = 0, pid = 0;
+    while (i < N)
+    {                    /* schedule and run maximum N jobs */
+        n = selectJob(); /* pick a job from the job priority queues */
+        if (n > 0)
+        { /* valid job id */
+            if (pid = fork() == 0)
+            {                  /* child worker process */
+                executeJob(n); /* execute the job */
+                exit(0);
+            }
+        }
+        i++;
+    }
+}
+
+int selectJob()
+{
+    int n = 1;
+    cout << "selectJob: Select a highest priority job from the priority queue: \n";
+    /* ... */
+    return n;
+}
+
+void executeJob(int n)
+{
+    if (n >= 1 && n <= 30)
+    {
+        cout << "executeJob: execute server job " << n << endl;
+        /* ... */
+    }
+    else if (n >= 31 && n <= 60)
+    {
+        cout << "executeJob: execute power user job " << n << endl;
+        /* ... */
+    }
+    else if (n >= 61 && n <= 100)
+    {
+        cout << "executeJob: execute user job " << n << endl;
+        /* ... */
+    }
+}
