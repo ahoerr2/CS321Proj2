bbe676e4812c67b91b88417c2eca21c1cfff4200
ahoerr2 <ahoerr2@gmail.com>
2021-11-10 14:36:14 -0600

    I got it

--- Main.cpp
+++ Main.cpp
@@ -21,7 +21,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define N 20 //N is the number of the worker processes. You may increase N to 100 when your program runs correctly
+#define N 10 //N is the number of the worker processes. You may increase N to 100 when your program runs correctly
 #define M 10 //M is the number of jobs. You may increase M to 50 when your program runs correctly
 #define debug 1
 using namespace std;
@@ -38,16 +38,16 @@ void setJobQueues();
 void jobGenerator();
 void jobScheduler();
 string selectJob();
-void executeJob(int n, const string &token);
+void executeJob(int n);
 void down(int *semid, char *semname);
 void up(int semid, char *semname);
 
 const string SERVER_QUEUE = "queueServerFile";
 const string POW_USER_QUEUE = "queuePUserFile";
 const string USER_QUEUE = "queueRUserFile";
-const int MAX_SERVER_QUEUE = 1;
-const int MAX_POW_USER_QUEUE = 2;
-const int MAX_USER_QUEUE = 1;
+const int MAX_SERVER_QUEUE = 5;
+const int MAX_POW_USER_QUEUE = 5;
+const int MAX_USER_QUEUE = 5;
 
 int serverFileSize = 0;
 int pUserFileSize = 0;
@@ -73,16 +73,20 @@ int main()
     if (pid = fork() > 0)
     {                   /* jobGenerator process */
         jobGenerator(); /* generate random jobs and put them into the priority queues. The priority queues must be protected in a critical region */
+        while ((wpid = wait(&status)) > 0)
+        {
+            cout << "wait status: " << status << endl;
+        }
+        cout << "Exiting Program..." << endl;
+        exit(0);
     }
     else
     {                   /* job scheduler process */
         jobScheduler(); /* schedule and execute the jobs. */
         exit(0);
     }
-    while ((wpid = wait(&status)) > 0){
-    cout << "wait status: " << status << endl;
-        }
-    cout << "Exiting Program..." << endl;
+
+    
     
     return (1);
 }
@@ -190,12 +194,13 @@ void jobGenerator()
             up(semid, semname);
         }
 
-        usleep(100); //100 can be adjusted to synchronize the job generation and job scheduling processes.
+        usleep(10000); //100 can be adjusted to synchronize the job generation and job scheduling processes.
         i++;
     }
 }
 
-// The Scheduler schedules a number of jobs by first selecting a job from the highest priority queues
+// The Scheduler schedules a number of jobs by first selecting a job from the highest priority queues, then it splits the string to find the job number and the debug token
+// The number and token are then fed into a function that will detrimine the job priority level and execute job
 void jobScheduler()
 {
     string jobString;
@@ -203,11 +208,13 @@ void jobScheduler()
     int i = 0, n = 0, pid = 0;
     /* schedule and run maximum N jobs */
     while (i < N)
-    {                            
-        jobString = selectJob(); /* pick a job from the job priority queues */
+    {   /* pick a job from the job priority queues */
+        jobString = selectJob(); 
         //cout << "jobString: " << jobString << endl;
         n = stoi(jobString.substr(0, jobString.find('|')));
-        jobToken = jobString.substr(jobString.find('|') + 1, jobString.find(';') - jobString.find('|') - 1);
+        //jobToken = jobString.substr(jobString.find('|') + 1, jobString.find(';') - jobString.find('|') - 1);
+        
+        //Debug info to check how jobs are processed
         //cout << "N INDEX: " << n << endl;
         //cout << "TOKEN: " << jobToken << endl;
         
@@ -215,67 +222,79 @@ void jobScheduler()
         { /* valid job id */
             if (pid = fork() == 0)
             {                  /* child worker process */
-                executeJob(n,jobToken); /* execute the job */
+                executeJob(n); /* execute the job */
                 exit(0);
             }
+            i++;
         }
-        i++;
     }
+    // TODO: check control c is hit
 }
 
+
+// Selects a job from the highest priority queue (ORDER: SERVER, POW USER, USER), then returns the job string if a job was found or zero to skip the job if nothing was in queues
 string selectJob()
 {
+    // Filestream variables and variable to denote the extracted job
     fstream queueServer;
     fstream queuePUser;
     fstream queueRUser;
-    int index = 0;
     string job;
+
     cout << "selectJob: Select a highest priority job from the priority queue: \n";
 
+    // Puts down the semaphore and checks if there is any data in server queue, if so it will extract the top line of the queue, delete the line and return it
     down(&semid, semname);
     queueServer.open(SERVER_QUEUE, ios::in);
     if (getline(queueServer, job))
     {
         queueServer.close();
         up(semid,semname);
+
+        //The top line of the file is deleted, and the queue size is decremented
         down(&semid, semname);
         queueServer.open(SERVER_QUEUE, ios::in);
-        //cout << "server isn't empty!" << endl;
         popLineFromFile(queueServer, SERVER_QUEUE);
         serverFileSize--;
         queueServer.close();
         up(semid, semname);
+
         return job;
     }
     queueServer.close();
     up(semid, semname);
 
+    // Puts down the semaphore and checks if there is any data in pow user queue, if so it will extract the top line of the queue, delete the line and return it
     down(&semid, semname);
     queuePUser.open(POW_USER_QUEUE, ios::in);
     if (getline(queuePUser, job))
     {
         queuePUser.close();
         up(semid, semname);
+
+        //The top line of the file is deleted, and the queue size is decremented
         down(&semid, semname);
-        queuePUser.open(POW_USER_QUEUE, ios::in);
-        //cout << "pu isn't empty!" << endl;
+        queuePUser.open(POW_USER_QUEUE, ios::in);        
         popLineFromFile(queuePUser, POW_USER_QUEUE);
         pUserFileSize--;
         up(semid, semname);
+
         return job;
     }
     queuePUser.close();
     up(semid, semname);
 
+    // Puts down the semaphore and checks if there is any data in pow user queue, if so it will extract the top line of the queue, delete the line and return it
     down(&semid, semname);
     queueRUser.open(USER_QUEUE, ios::in);
     if (getline(queueRUser, job))
     {
         queueRUser.close();
         up(semid, semname);
+
+        //The top line of the file is deleted, and the queue size is decremented
         down(&semid, semname);
         queueRUser.open(USER_QUEUE, ios::in);
-        //cout << "u isn't empty!" << endl;
         popLineFromFile(queueRUser, USER_QUEUE);
         rUserFileSize--;
         up(semid, semname);
@@ -283,52 +302,56 @@ string selectJob()
     }
     up(semid, semname);
 
+    // If all the queues are empty zero is returned to skip executing the job
     return "0|NULL";
 }
 
+// Removes the top line from the file by copying every line from the queue file besides the top line to a temp queue
+// Then the old queue file is deleted and the temp file is renamed to the queues file name, effectively decrementing the queue
 void popLineFromFile(fstream &stream, const string streamFileName)
 {
-    // Creates a temp file of all of the passwords, inserts each line into the new file until it finds the username
-    // corresponding to the current user name. WHen it does it inserts the current User name and new password onto the line instead
+    // Creating the temp fileand variables to copy
     fstream tempFile;
     tempFile.open("temp", ios::out);
     int index = 0;
     string streamLine = "";
     string ret = "";
-    //cout << "Seletcting job..." << endl;
+
+    // Copy every line to the temp file but the first line
     while (getline(stream, streamLine))
     {
-        //cout << "string: " << streamLine << endl;
         if (index == 0)
         {
-            //cout << "skip line: " << streamLine << endl;
             ++index;
             continue;
         }
-        //cout << "string add back in: " << streamLine << endl;
         tempFile << streamLine << endl;
         index++;
     }
     tempFile.close();
 
-    // The old password file is deleted and the copies password file becomes the new password file.
+    // The old queue file is deleted and the copied queue file becomes the new queue file.
     remove(streamFileName.c_str());
     rename("temp", streamFileName.c_str());
 }
 
-void executeJob(int n, const string& token)
+// Determines what priority the job is by its job number then executes a specific job routiene for each job priority
+void executeJob(int n)
 {
+    // Runs server queue job, which prints out its pid and exits
     if (n >= 1 && n <= 30)
     {
         cout << "executeJob: execute server job " << n << endl;
         cout << "server job pid is: " << getpid() << endl;
         runTaskCode(n, SERVER_QUEUE);
     }
+    // Runs power user queue job, which waits for control C signal for user and then prints that it has woken up and exits
     else if (n >= 31 && n <= 60)
     {
         cout << "executeJob: execute power user job " << n << endl;
         runTaskCode(n, POW_USER_QUEUE);
     }
+    // Runs user queue job, which prints job id, sleeps for two seconds, and then prints its woken up and exits
     else if (n >= 61 && n <= 100)
     {
         cout << "executeJob: execute user job " << n << endl;
@@ -336,6 +359,7 @@ void executeJob(int n, const string& token)
     }
 }
 
+// Responsible for the executing each specific routiens of each job priority
 void runTaskCode(const int& jobID, const string& queuePriority){
     if(queuePriority == SERVER_QUEUE){
         cout << "Pid: " << getpid() << endl;
@@ -347,6 +371,7 @@ void runTaskCode(const int& jobID, const string& queuePriority){
         signal(SIGINT, wake_up);
         while(!intr); //wait for the ^C to wake up
         cout << "Power User Process has woken up" << endl;
+        bool signalCleared = true;
     }
     else if (queuePriority == USER_QUEUE)
     {
@@ -357,6 +382,7 @@ void runTaskCode(const int& jobID, const string& queuePriority){
     }
 }
 
+// Part of the semaphore system, sets the mutex into a down lock position that doesnt let other programs enter the critical region
 void down(int *semid, char *semname)
 {
     while (*semid = creat(semname, 0) == -1) /* && error == EACCES)*/
@@ -366,13 +392,15 @@ void down(int *semid, char *semname)
     }
 }
 
+// Part of the semaphore system, sets the mutex into a up unlock position that let other programs enter a critical region
 void up(int semid, char *semname)
 {
     close(semid);
     unlink(semname);
-    //cout << "up " << semname << ": I am waked up.\n";
+    cout << "up " << semname << ": I am waked up.\n";
 }
 
+// When the signal is inputted an integer is set to one which allows all programs waiting on the signal to exit
 void wake_up(int s)
 {
     cout << "\nI will wake up now.\n";
